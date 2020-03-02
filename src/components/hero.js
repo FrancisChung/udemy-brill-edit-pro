f576e4a59a9248c10136d6ca51fc56e03c9aad87
FrancisChung <francis.c.chung@gmail.com>
2020-03-03 10:01:45 +1100

    Background image displaying

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import {RichText} from 'prismic-reactjs'
+import styled from 'styled-components'
+
+const HeroWrapper = styled.section`
+    background:url('${props=>props.backgroundImage}');
+`
+
 
 const Hero = ({title, content, backgroundImage}) => {
     console.log(title, content, backgroundImage);
     return (
-        <div>
+        <HeroWrapper backgroundImage={backgroundImage}>
             <RichText render={title} />
             <p>
                 {content[0].text}
             </p>
-        </div>
+        </HeroWrapper>
     );
 }
 
