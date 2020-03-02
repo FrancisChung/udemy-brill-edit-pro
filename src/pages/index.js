bc4896b4a7fc5a0289c5ef80b169cfab46c23d46
FrancisChung <francis.c.chung@gmail.com>
2020-03-02 12:09:11 +1100

    Incorporate SliceZone component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import {graphql} from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import SliceZone from "../components/sliceZone"
 
 export const query = graphql`
 {
@@ -28,18 +27,10 @@ export const query = graphql`
 `
 
 const IndexPage = (props) => {
-  console.log(props);
   return(
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+      <SliceZone body={props.data.prismic.allHomepages.edges[0].node.body} />
+    </Layout>
   );
 }
 
