5433248c855dc264210ae187c69f012892fbf536
FrancisChung <francis.c.chung@gmail.com>
2020-03-03 10:50:34 +1100

    HeroWrapper styled

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,17 +4,30 @@ import styled from 'styled-components'
 
 const HeroWrapper = styled.section`
     background:url('${props=>props.backgroundImage}');
+    background-size:cover;
+    background-repeat: no-repeat;
+    height: calc(100vh - 66px);
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: white;
+
+    div {
+        max-width: 800px;
+        margin: 0 auto;
+        background: rgba(0,0,0,0.5);
+    }
 `
-
-
 const Hero = ({title, content, backgroundImage}) => {
     console.log(title, content, backgroundImage);
     return (
         <HeroWrapper backgroundImage={backgroundImage}>
-            <RichText render={title} />
-            <p>
-                {content[0].text}
-            </p>
+            <div>
+                <RichText render={title} />
+                <p>
+                    {content[0].text}
+                </p>
+            </div>
         </HeroWrapper>
     );
 }
