f1d4a4d72bcb56a6a235699ee15fd69dcf44cf7e
FrancisChung <francis.c.chung@gmail.com>
2020-03-02 12:08:32 +1100

    New Hero.js component

---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {RichText} from 'prismic-reactjs'
+
+const Hero = ({title, content}) => {
+    console.log(title, content);
+    return (
+        <div>
+            <RichText render={title} />
+            <p>
+                {content}
+            </p>
+        </div>
+    );
+    }
+
+export default Hero;
