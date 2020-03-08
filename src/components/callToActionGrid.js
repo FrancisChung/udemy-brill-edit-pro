904f03980389788fad6d0097cf512d6f3d817a1e
FrancisChung <francis.c.chung@gmail.com>
2020-03-08 11:28:11 +1100

    Using CallToActionBlock in Grid

--- src/components/callToActionGrid.js
+++ src/components/callToActionGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import {RichText} from 'prismic-reactjs'
+import {RichText} from 'prismic-reactjs';
+import CallToActionBlock from './callToActionBlock';
 
 const CallToActionGridWrapper = styled.section`
     max-width: 800px;
@@ -14,9 +15,11 @@ const CallToActionGrid = ({title, callToActions}) => {
             <RichText render={title} />
             {callToActions.map((callToAction,i) => {
                 return (
-                    <div key={i}>
-                        Call To Action
-                    </div>
+                    <CallToActionBlock 
+                        key={i}
+                        title={callToAction.call_to_action_title}
+                        content={callToAction.content}
+                    />
                 )
             })}
         </CallToActionGridWrapper>
