5072fcf937138114dc27352c6c96c1f46c014051
FrancisChung <francis.c.chung@gmail.com>
2020-03-12 21:24:28 +1100

    Fixed styling for 3 tiers to appear next row.

--- src/components/priceList.js
+++ src/components/priceList.js
@@ -6,22 +6,27 @@ import PriceItem from './priceItem';
 const PriceListWrapper= styled.section`
     max-width: 800px;
     margin: 0 auto;
-
+    div:last-child {
+        display: flex;
+    }
 `
 
 const PriceList = ({title, prices}) => {
     return (
         <PriceListWrapper>
             <RichText render={title} />
+            <div>
             {prices.map((price, i) => {
                 return (
                     <PriceItem 
                         key={i}
                         title={price.price_list_title}
+                        price={price.price_per_month}
                         description={price.price_list_description}
                     />
                 )
             })}
+            </div>
         </PriceListWrapper>
     )
 }
