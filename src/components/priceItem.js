244f12171953a3890aefd245d33669ac33e8f8b3
FrancisChung <francis.c.chung@gmail.com>
2020-03-12 21:23:36 +1100

    Fleshig out PriceItem

--- src/components/priceItem.js
+++ src/components/priceItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import {RichText} from 'prismic-reactjs';
 
-const PriceItem = ({title}) => {
+const PriceItemWrapper = styled.div`
+    flex-grow: 1;
+    flex-basis: 0;
+    margin: 0 10px;
+`
+
+const PriceItem = ({title, price, description}) => {
     return (
-        <div>
+        <PriceItemWrapper>
             <RichText render={title} />
-        </div>
+            <div className="price">
+                {price} <span className="duration">/ month</span>
+            </div>
+            <RichText render={description} />
+        </PriceItemWrapper>
     )
 
 }
